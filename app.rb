ddd2731c28f7d64b7c29c1040071fcd32069e697
TomStuart92 <Thomascstuart@gmail.com>
2016-08-31 13:40:11 +0100

    Post-Redirect-Get loop

--- app.rb
+++ app.rb
@@ -1,17 +1,22 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-  set :sessions, true
-  set :foo, 'bar'
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
+  get '/play' do
+    @player1 = session[:Player1]
+    @player2 = session[:Player2]
+    erb(:play)
+  end
+
   post '/names' do
-      @player1 = params[:Player1]
-      @player2 = params[:Player2]
-      erb(:names)
+    session[:Player1] = params[:Player1]
+    session[:Player2] = params[:Player2]
+    redirect to('/play')  
   end
 
   run! if app_file == $0
