0fd42fa37398699014955cd95f534bf0ba93b005
TomStuart92 <Thomascstuart@gmail.com>
2016-08-31 14:38:49 +0100

    Add Web Helper

--- spec/features/get_name_spec.rb
+++ spec/features/get_name_spec.rb
@@ -3,10 +3,7 @@ require 'spec_helper'
 RSpec.feature 'get user name' do
 
   scenario 'with valid credentials' do
-    visit '/'
-    fill_in 'Player1', with: "Bob"
-    fill_in 'Player2', with: "John"
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content "Hello Bob & John"
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.feature 'view hitpoints' do
 
   scenario 'current hitpoint status' do
-    visit '/play'
+    sign_in_and_play
     expect(page).to have_content "Player1:100hp&Player2:100hp"
   end
 end
