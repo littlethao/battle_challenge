4c635cd14663d7277563b9c909532a0ae3e307e6
Thao Vo <ttthao.vo@gmail.com>
2016-09-02 11:09:39 +0100

    Player 2 attacks back

--- battle/spec/features/attack_spec.rb
+++ battle/spec/features/attack_spec.rb
@@ -26,5 +26,13 @@ feature 'Attacking' do
       click_button 'Attack'
       expect(page).to have_content "Current player is: Thao"
     end
+
+    scenario 'reduce hitpoint by 10 of player 1' do
+      sign_in_and_play
+      click_button 'Attack'
+      click_button 'Attack'
+      expect(page).not_to have_content "Matt: 100"
+      expect(page).to have_content "Matt: 90"
+    end
   end
 end
